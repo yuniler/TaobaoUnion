fd9deb3cd976f777b44e5bb3c572ff1e78b3e638
sunguangyuniler@163.com <sunguangyuniler@163.com>
2021-01-31 21:02:19 +0800

    taobaoUnionFirst\

---
+++ app/src/main/java/cn/jkdev/taobaounion/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.jkdev.taobaounion.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.jkdev.taobaounion.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
